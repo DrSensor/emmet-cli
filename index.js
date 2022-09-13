00b1a363622729c87b2484ffae8acf9a14635aad
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2022-09-13 15:55:33 +0700

    support reading abbreviation from stdin

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import expand from './emmet.js';
-import { printf, exit } from 'std';
+import { printf, in as stdin, exit } from 'std';
+import { isatty } from "os";
 
-if (scriptArgs.length === 1 || scriptArgs[1] === "-h" || scriptArgs[1] === "--help")  {
-	printf(`usage: ${scriptArgs[0]} 'emmet-abbreviation'\n`)
-	exit(0)
+if (!isatty(stdin)) scriptArgs[1] = stdin.getline()
+else if (scriptArgs.length === 1 || scriptArgs[1] === "-h" || scriptArgs[1] === "--help")  {
+    printf(`usage: ${scriptArgs[0]} 'emmet-abbreviation'\n`)
+    exit(0)
 }
 
 const expanded = expand(scriptArgs[1]);
